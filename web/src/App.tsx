8744f2aa62644d9fa4a255479ecd75b8cec17817
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-06-29 13:33:43 -0400

    map with ts syntax to avoid error

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,31 +1,31 @@
-import { useEffect } from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 import './App.css';
 
 export function App() {
+  const [repos, setRepos] = useState('');
+
   useEffect(() => {
-    axios.get('http://localhost:4000/repos').then((res) => {
-      console.log(res.data);
-    });
+    getRepos();
   }, []);
+
+  const getRepos = () => {
+    axios.get('http://localhost:4000/repos').then((res) => {
+      console.log(res);
+      setRepos(res.data);
+    });
+  };
+  console.log(repos);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {repos && (
+        <div>
+          {(repos as unknown as any[]).map((x: any) => (
+            <div key={x.id}>{x.name}</div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
