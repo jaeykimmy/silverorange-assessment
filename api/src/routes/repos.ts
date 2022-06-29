05b7799c7fc018c1b40157f3e7b7024979b279a8
Jaeyoung Kim <j.jaeykim@gmail.com>
2022-06-29 11:22:52 -0400

    return repositories where repository.fork === false

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -2,12 +2,12 @@ import { Router, Request, Response } from 'express';
 import data = require('../../data/repos.json');
 
 export const repos = Router();
-console.log(data);
+
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
 
   res.status(200);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  return res.json(data.filter((element) => element.fork === false));
 });
